98003966e27df59a1d69029f845b12d24f3df34d
Slemgrim <slemgrim@gmail.com>
2016-04-24 12:15:04 +0200

    add base url to all endpoints

--- src/api.js
+++ src/api.js
@@ -1,5 +1,11 @@
 import config from '../config.json';
 
 export function baseUrl(){
-    return config.protocol + '://' + config.host + ':' + config.port;
+    let url = config.protocol + '://' + config.host + ':' + config.port;
+
+    if(config.base != '/'){
+        url += config.base;
+    }
+
+    return url;
 }
